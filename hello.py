56f5e10bda2af504daa602ab21c3b0b4bb20a9f2
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 17:51:10 -0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
 
 name = input("what is your name? ")
 print("Hello", name + "!")
+
+city = input("which city are you from? ")
+
+if city == "San Francisco":
+    print("Me too!")
+
+else:
+    print(city, "is a nice city!")
